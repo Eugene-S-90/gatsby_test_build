24790038121f77460594c2dd4fefb8a1bc0d60d9
Eugene Sokolyansky <evgenys@123software.ru>
2019-05-13 16:39:16 +0300

    lol1

--- src/pages/lol.js
+++ src/pages/lol.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import iconImg from "../components/iconImg"
+import IconImg from "../components/iconImg"
 
 const lol = () => (
   <Layout>
@@ -11,7 +11,7 @@ const lol = () => (
     <h1>Hi from the seconsadasd page</h1>
     <p>Welcome to sadaspage sadas2</p>
     <div className="img-wrapper">
-    <iconImg />
+    <IconImg />
     </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
