9110598c526fced2f7df1c0748013079717e5cd2
Eugene Sokolyansky <evgenys@123software.ru>
2019-05-13 15:26:18 +0300

    first commit

---
+++ src/pages/test.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const test = () => (
+  <Layout>
+    <SEO title="Page 3" />
+    <h1>Hi from the seconsadasd page</h1>
+    <p>Welcome to sadaspage sadas2</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default test
