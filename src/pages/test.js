ba9335b8417c635db9f8d796e42856e623973718
Eugene Sokolyansky <evgenys@123software.ru>
2019-05-13 16:24:05 +0300

    second test comm

--- src/pages/test.js
+++ src/pages/test.js
@@ -3,12 +3,16 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Image from "../components/iconimg"
 
 const test = () => (
   <Layout>
     <SEO title="Page 3" />
     <h1>Hi from the seconsadasd page</h1>
     <p>Welcome to sadaspage sadas2</p>
+    <div class="img-wrapper">
+    <Image/>
+    </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
